8af048c6a26bf361e008b6ebc6979ef03c865b0b
qcodelabsllc <84394923+quabynah-bilson@users.noreply.github.com>
2023-05-14 21:36:36 +0000

    Updated button props

--- lib/src/widgets/app.rounded.button.dart
+++ lib/src/widgets/app.rounded.button.dart
@@ -20,10 +20,11 @@ class AppRoundedButton extends StatelessWidget {
   final bool enabled;
   final bool outlined;
   final IconData? icon;
+  final Gradient? gradient;
 
   const AppRoundedButton({
     Key? key,
-    this.elevation = 5,
+    this.elevation = 2,
     required this.text,
     required this.onTap,
     this.buttonType = AppButtonType.primary,
@@ -34,6 +35,7 @@ class AppRoundedButton extends StatelessWidget {
     this.outlined = false,
     this.icon,
     this.iconLocation = IconLocation.start,
+    this.gradient,
   }) : super(key: key);
 
   @override
@@ -53,14 +55,14 @@ class AppRoundedButton extends StatelessWidget {
         (context.theme.buttonTheme.shape as RoundedRectangleBorder)
             .borderRadius;
 
-
     return SizedBox(
       width: buttonWidth,
       child: outlined
           ? GestureDetector(
               onTap: enabled ? onTap : null,
               child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 14),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
                 clipBehavior: Clip.hardEdge,
                 decoration: BoxDecoration(
                   borderRadius: buttonShape,
@@ -70,7 +72,8 @@ class AppRoundedButton extends StatelessWidget {
                             (buttonType == AppButtonType.primary
                                 ? context.colorScheme.primary
                                 : context.colorScheme.secondary)
-                        : context.theme.disabledColor,
+                        : context.theme.disabledColor
+                            .withOpacity(kEmphasisLowest),
                     width: 2,
                   ),
                 ),
@@ -88,7 +91,8 @@ class AppRoundedButton extends StatelessWidget {
                                 (buttonType == AppButtonType.primary
                                     ? context.colorScheme.primary
                                     : context.colorScheme.secondary)
-                            : context.theme.disabledColor,
+                            : context.theme.disabledColor
+                                .withOpacity(kEmphasisLowest),
                       ).right(12),
                     },
                     text.button(
@@ -100,7 +104,7 @@ class AppRoundedButton extends StatelessWidget {
                                   ? context.colorScheme.primary
                                   : context.colorScheme.secondary)
                           : context.theme.disabledColor,
-                      emphasis: enabled ? kEmphasisHighest : kEmphasisMedium,
+                      emphasis: enabled ? kEmphasisHighest : kEmphasisLow,
                     ),
                     if (icon != null && iconLocation == IconLocation.end) ...{
                       Icon(
@@ -120,64 +124,81 @@ class AppRoundedButton extends StatelessWidget {
             )
           : GestureDetector(
               onTap: enabled ? onTap : null,
-              child: Material(
-                clipBehavior: Clip.hardEdge,
-                elevation: enabled ? elevation : 0,
-                shape: context.theme.buttonTheme.shape,
-                color: enabled
-                    ? backgroundColor ??
-                        (buttonType == AppButtonType.primary
-                            ? context.colorScheme.primary
-                            : context.colorScheme.secondary)
-                    : context.theme.disabledColor,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      if (icon != null &&
-                          iconLocation == IconLocation.start) ...{
-                        Icon(
-                          icon,
-                          size: context.textTheme.labelLarge!.fontSize! * 1.4,
-                          color: enabled
-                              ? textColor ??
-                                  (buttonType == AppButtonType.primary
-                                      ? context.colorScheme.onPrimary
-                                      : context.colorScheme.onSecondary)
-                              : context.theme.disabledColor,
-                        ).right(12),
-                      },
-                      text.button(
-                        context,
-                        alignment: TextAlign.center,
-                        color: enabled
-                            ? textColor ??
-                                (buttonType == AppButtonType.primary
-                                    ? context.colorScheme.onPrimary
-                                    : context.colorScheme.onSecondary)
-                            : context.theme.disabledColor,
-                        emphasis: enabled ? kEmphasisHighest : kEmphasisMedium,
+              child: gradient == null
+                  ? Material(
+                      clipBehavior: Clip.hardEdge,
+                      elevation: enabled ? elevation : 0,
+                      shape: context.theme.buttonTheme.shape,
+                      color: enabled
+                          ? backgroundColor ??
+                              (buttonType == AppButtonType.primary
+                                  ? context.colorScheme.primary
+                                  : context.colorScheme.secondary)
+                          : context.theme.disabledColor
+                              .withOpacity(kEmphasisLowest),
+                      child: _buildFilledBottom(context),
+                    )
+                  : Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: buttonShape,
+                        gradient: gradient,
                       ),
-                      if (icon != null && iconLocation == IconLocation.end) ...{
-                        Icon(
-                          icon,
-                          size: context.textTheme.labelLarge!.fontSize! * 1.4,
-                          color: enabled
-                              ? textColor ??
-                                  (buttonType == AppButtonType.primary
-                                      ? context.colorScheme.onPrimary
-                                      : context.colorScheme.onSecondary)
-                              : context.theme.disabledColor,
-                        ).left(12),
-                      },
-                    ],
-                  ),
-                ),
-              ),
+                      child: Material(
+                        clipBehavior: Clip.hardEdge,
+                        elevation: enabled ? elevation : 0,
+                        shape: context.theme.buttonTheme.shape,
+                        color: Colors.transparent,
+                        child: _buildFilledBottom(context),
+                      ),
+                    ),
             ),
     );
   }
+
+  Widget _buildFilledBottom(BuildContext context) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            if (icon != null && iconLocation == IconLocation.start) ...{
+              Icon(
+                icon,
+                size: context.textTheme.labelLarge!.fontSize! * 1.4,
+                color: enabled
+                    ? textColor ??
+                        (buttonType == AppButtonType.primary
+                            ? context.colorScheme.onPrimary
+                            : context.colorScheme.onSecondary)
+                    : context.theme.disabledColor.withOpacity(kEmphasisLowest),
+              ).right(12),
+            },
+            text.button(
+              context,
+              alignment: TextAlign.center,
+              color: enabled
+                  ? textColor ??
+                      (buttonType == AppButtonType.primary
+                          ? context.colorScheme.onPrimary
+                          : context.colorScheme.onSecondary)
+                  : context.theme.disabledColor,
+              emphasis: enabled ? kEmphasisHighest : kEmphasisLow,
+            ),
+            if (icon != null && iconLocation == IconLocation.end) ...{
+              Icon(
+                icon,
+                size: context.textTheme.labelLarge!.fontSize! * 1.4,
+                color: enabled
+                    ? textColor ??
+                        (buttonType == AppButtonType.primary
+                            ? context.colorScheme.onPrimary
+                            : context.colorScheme.onSecondary)
+                    : context.theme.disabledColor,
+              ).left(12),
+            },
+          ],
+        ),
+      );
 }
