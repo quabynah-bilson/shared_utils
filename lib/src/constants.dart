ca558ead1cdbbd4b1b0c748f2101b5c8a4a6b5a3
quabynah-bilson <84394923+quabynah-bilson@users.noreply.github.com>
2023-07-29 22:50:49 +0000

    Update logger setup and import statements in constants.dart
    Added import for 'package:flutter/foundation.dart' for enabling different logger levels depending on the debug mode. Updated the logger setup to include PrettyPrinter and to set the level to verbose during debug mode and nothing otherwise. This allows for more detailed logging during development and silent logging in production.

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,15 +1,21 @@
 library shared_utils;
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:logger/logger.dart';
 
 /// anim
-const kDefaultLottieLoadingAnim = 'https://assets4.lottiefiles.com/packages/lf20_poqmycwy.json';
+const kDefaultLottieLoadingAnim =
+    'https://assets4.lottiefiles.com/packages/lf20_poqmycwy.json';
+
 /// fonts
 TextTheme kPrimaryFonts = GoogleFonts.dmSansTextTheme();
 TextStyle kPrimaryTextStyle = GoogleFonts.dmSans();
 TextTheme kSecondaryFonts = GoogleFonts.rubikTextTheme();
 
 // debugging
-final logger = Logger();
+final logger = Logger(
+  printer: PrettyPrinter(),
+  level: kDebugMode ? Level.verbose : Level.nothing,
+);
